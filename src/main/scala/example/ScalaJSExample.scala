008d8672e3c64acade4b560329281a89a742e015
Michael Nash <mnash@jglobal.com>
2014-06-07 13:51:15 -0600

    Given up on the direct type mapping - extending wrapper classes

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -3,51 +3,30 @@ package example
 import scala.scalajs.js
 import js.annotation.JSExport
 import org.scalajs.dom
-import org.scalajs.dom.HTMLElement
 import scala.scalajs.js.annotation.JSName
 import js.Dynamic.{ global => g, newInstance => jsnew, literal => lit }
-
-@JSName("Apple")
-trait Apple extends js.Object {
-  val `type`: String = ???
-  val color: String = ???
-}
 
 @JSName("THREE.Scene")
 class ThreeScene
 
-@JSName("BorderContainer")
-trait BorderContainer extends js.Object {
-}
 
-@JSName("ContentPane")
-trait ContentPane extends js.Object {
-  var region: String = ???
-}
 
 class Grid(id: String, columns: List[(String, String)]) {
-   def columns(field1: String, label1: String) = Array(lit("field" -> field1, "label" -> label1))
-   def columns(field1: String, label1: String, field2: String, label2: String) =
-     Array(lit("field" -> field1, "label" -> label1), lit("field" -> field2, "label" -> label2))
-   def columns(field1: String, label1: String, field2: String, label2: String, field3: String, label3: String) =
-     Array(lit("field" -> field1, "label" -> label1), lit("field" -> field2, "label" -> label2), lit("field" -> field3, "label" -> label3))
-   private var wrapped: js.Dynamic = null
+    g.require(Array[String]("dgrid/Grid", "dojo/domReady!"), { (grid: js.Dynamic) =>
 
-   g.require(Array[String]("dgrid/Grid", "dojo/domReady!"), { (grid: js.Dynamic) =>
+      val rows = js.Array(js.Dictionary("first" -> "Fred", "last" -> "Barking", "age" -> 89),
+        js.Dictionary("first" -> "Vanna", "last" -> "Green", "age" -> 55))
 
-      val data = Array(
-        js.Dynamic.literal(first = "Fred", last = "Barkers", age = 89),
-        js.Dynamic.literal(first = "Vanna", last = "Blue", age = 55),
-        js.Dynamic.literal(first = "Pat", last = "Sajak", age = 65)
-      )
+      val cols = js.Dictionary("columns" -> js.Dictionary("first" -> "First Name", "last" -> "Last Name", "age" -> "Age"))
 
-      val cols = lit("columns" -> columns("first", "First Name", "last", "Last Name", "age", "Age"))
+      g.console.log(rows)
 
-      val g = jsnew(grid)(cols, id)
-      g.renderArray(data)
-      wrapped = g
+      val gr = jsnew(grid)(cols, "grid2")
+
+    gr.columns = cols
+      gr.renderArray(rows)
     })
-  def renderArray(e: Array[js.Dynamic]) = ???
+    def renderArray(data: List[(String, String)]) = ???
 }
 
 @JSExport
@@ -73,12 +52,9 @@ object ScalaJSExample {
 
     val threeS = scene.asInstanceOf[ThreeScene]
 
-    dom.alert("Here")
+   // dom.alert("Here")
 
-    val apple = jsnew(g.Apple)("type", "red")
-    val a = apple.asInstanceOf[Apple]
 
-    dom.alert(a.color)
 
     val bc = dom.document.createElement("div")
     bc.id = "bordercontainer"
@@ -97,51 +73,39 @@ object ScalaJSExample {
      cont1.startup()
    })
 
-    grid2()
+    new Grid("foo", null)
+    //grid2()
 
   }
 
-  def grid2() {
-    g.require(Array[String]("dgrid/Grid", "dojo/domReady!"), { (grid: js.Dynamic) =>
-      val dat = Array(Map("first" -> "Fred", "last" -> "Barker", "age" -> 89))
-      val data = Array(
-        lit("first" -> "Fred", "last" -> "Barkers", "age" -> 89),
-        lit(first = "Vanna", last = "Blue", age = 55),
-        lit(first = "Pat", last = "Sajak", age = 65)
-      )
-
-      val cols = Array(new ColumnDef("first", "First Name"), new ColumnDef("last", "Last Name"), new ColumnDef("age", "Age"))
-      val cd = new Columns(cols)
-      val gridCall = js.Dynamic.literal(columns = js.Dynamic.literal(first = "First Name", last = "Last Name", age = "Age"))
-
-      val g = new OnDemandGrid(cd, "grid2")
-
-      g.renderArray(data)
-    })
-  }
-
-  /** Computes the square of an integer.
-   *  This demonstrates unit testing.
-   */
-  def square(x: Int): Int = x*x
+//  def grid2() {
+//    g.require(Array[String]("dgrid/Grid", "dojo/domReady!"), { (grid: js.Dynamic) =>
+//      val dat = Array(Map("first" -> "Fred", "last" -> "Barker", "age" -> 89))
+//      val data = Array(
+//        lit("first" -> "Fred", "last" -> "Barkers", "age" -> 89),
+//        lit(first = "Vanna", last = "Blue", age = 55),
+//        lit(first = "Pat", last = "Sajak", age = 65)
+//      )
+//
+//    val row = Array(js.Dictionary("first" -> "Fred", "last" -> "Barking", "age" -> 89))
+//
+//      val cols = Array(new ColumnDef("first", "First Name"), new ColumnDef("last", "Last Name"), new ColumnDef("age", "Age"))
+//      val cd = (new Columns(cols)).asInstanceOf[js.Dynamic]
+//      val gridCall = lit(columns = lit(first = "First Name", last = "Last Name", age = "Age"))
+//
+//      val gr = jsnew(grid)(gridCall, "grid2")
+//      gr.renderArray(row)
+//    })
+//  }
+//
+//  /** Computes the square of an integer.
+//   *  This demonstrates unit testing.
+//   */
+//  def square(x: Int): Int = x*x
 }
 
-@JSExport
-class ColumnDef(_field: String, _label: String) {
-  @JSExport("field")
-  def field: String = _field
-  @JSExport("label")
-  def label: String = _label
-}
-
-@JSExport
-class Columns(_columns: Array[ColumnDef]) {
-  def columns: Array[ColumnDef] = _columns
-}
-
-@JSName("dgrid.OnDemandGrid")
-class OnDemandGrid(columns: Columns, id: String) extends js.Object {
-  def renderArray(data: Any) = ???
-}
-
-
+//@JSExport
+//class ColumnDef(field: String, label: String)
+//
+//@JSExport
+//class Columns(columns: Array[ColumnDef])
