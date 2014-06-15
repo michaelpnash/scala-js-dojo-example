1b7b10ed3e01f9ab431d1ec772d2cf45793e3a4c
Michael Nash <mnash@jglobal.com>
2014-06-14 21:29:34 -0600

    Adding editor config class for store

--- src/main/scala/dojo/store/JsonRest.scala
+++ src/main/scala/dojo/store/JsonRest.scala
@@ -4,6 +4,10 @@ import scala.scalajs.js
 
 import js.Dynamic.{newInstance => jsnew}
 
+case class EditorArgs(store: DataStore, searchAttr: String)
+
+case class Editor(label: String, field: String, editor: js.Any, editorArgs: EditorArgs, editOn: String, autoSave: Boolean, width: Int)
+
 trait DataStore extends js.Object
 
 trait JsonRest extends DataStore {
